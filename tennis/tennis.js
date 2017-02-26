7394527c9bac579ffdc5b8539821cce10a9428c0
richardyam1 <richardyam1@yahoo.com>
2017-02-26 13:39:10 -0500

    First Commit

---
+++ tennis/tennis.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    var canvas;
+    var convasContext;
+    console.log("Hello World!");
+
+    canvas = document.getElementById("gameCanvas");
+    convasContext = canvas.getContext("2d");
+});
