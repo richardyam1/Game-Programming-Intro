ec1d8cdbd51a2cb2b08cf0ee3460ebea91b4684e
richardyam1 <richardyam1@yahoo.com>
2017-03-10 17:03:11 -0500

    Create board and ball

--- brickbreak/brickbreak.js
+++ brickbreak/brickbreak.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
+	var canvas;
+	var convasContext;
 
+	canvas = document.getElementById("gameCanvas");
+	canvasContext = canvas.getContext("2d");
+	drawEverything();
+	
+	function drawEverything(){
+		//board
+		canvasContext.fillStyle = "black";
+		canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+
+		//draw ball
+		canvasContext.fillStyle = "white";
+		canvasContext.beginPath();
+		canvasContext.arc(75, 75, 10, 0, Math.PI*2, true);
+		canvasContext.fill();
+	}
 });
