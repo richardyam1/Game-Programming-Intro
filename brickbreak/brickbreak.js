d3c7549a93d6b6f4fc418bfcff12f4f451b23622
richardyam1 <richardyam1@yahoo.com>
2017-03-16 12:15:30 -0400

    Gaps between bricks no longer send ball backwards.  Restore dimensions of brick to original values

--- brickbreak/brickbreak.js
+++ brickbreak/brickbreak.js
@@ -98,9 +98,9 @@ $(document).ready(function(){
 		//moves ball vertically
 		ballY += ballSpeedY;
 
-		if(checkForAndRemoveBrickAtPixelCoord(ballX, ballY)){
-			ballSpeedY *= -1;
-		}
+		breakAndBounceOffBrickAtPixelCoord(ballX, ballY);
+			
+		
 	}
 
 	
@@ -111,7 +111,7 @@ $(document).ready(function(){
 		}
 	}
 
-	function checkForAndRemoveBrickAtPixelCoord(pixelX, pixelY){
+	function breakAndBounceOffBrickAtPixelCoord(pixelX, pixelY){
 		var tileCol = pixelX/BRICK_W;
 		var tileRow = pixelY/BRICK_H;
 
@@ -121,14 +121,49 @@ $(document).ready(function(){
 
 		//first check whether the ball is within any part of the brick wall
 		if(tileCol < 0 || tileCol >= BRICK_COLS || tileRow < 0 || tileRow >= BRICK_ROWS){
-			return; // bail out of function to avoid illegal array position usage
+			return false; // bail out of function to avoid illegal array position usage
 		}
 
-		if(isBrickAtTileCoord(tileCol, tileRow)){
-			var brickIndex = brickTileToIndex(tileCol, tileRow);
+		var brickIndex = brickTileToIndex(tileCol, tileRow);
+
+		if(brickGrid[brickIndex] === 1){
+			//Checks the previous col or row of the ball
+			var prevBallX = ballX - ballSpeedX;
+			var prevBallY = ballY - ballSpeedY;
+			var prevTileCol = Math.floor(prevBallX / BRICK_W);
+			var prevTileRow = Math.floor(prevBallY / BRICK_H);
+
+			var bothTestsFailed = true;
+
+			//must come in horizontally
+			if(prevTileCol != tileCol){
+				var adjacentBrickIndex = brickTileToIndex(prevTileCol, tileRow);
+				//make sure the side we want to reflect off isn't blocked
+				if(brickGrid[adjacentBrickIndex] != 1){
+					ballSpeedX *= -1;
+					bothTestsFailed = false;
+				}
+			}
+
+			if(prevTileRow != tileRow){
+				var adjacentBrickIndex = brickTileToIndex(tileCol, prevtileRow);
+				//make sure the side we want to reflect off isn't blocked
+				if(brickGrid[adjacentBrickIndex] != 1){
+					ballSpeedY *= -1;
+					bothTestsFailed = false;
+				}
+			}
+
+			// we hit an "armpit" on the inside corner, flip both to avoid going into it
+			if(bothTestsFailed){
+				ballSpeedX *= -1;
+				ballSpeedY *= -1;
+			}
+
 			brickGrid[brickIndex] = 0;
-			return true;
 		}
+
+		
 	}
 
 	function brickTileToIndex(tileCol, tileRow){
