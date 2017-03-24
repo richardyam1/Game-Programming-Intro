d0366d4c05597ded9c7cc7bd59837df21d3c9802
richardyam1 <richardyam1@yahoo.com>
2017-03-24 16:28:08 -0400

    Add images for goal, flag, and trees

--- racing/js/imageLoading.js
+++ racing/js/imageLoading.js
@@ -2,7 +2,9 @@
 	var carPic = document.createElement("img");
 	var trackPicRoad = document.createElement("img");
 	var trackPicWall = document.createElement("img");
-
+	var trackPicGoal = document.createElement("img");
+	var trackPicTree = document.createElement("img");
+	var trackPicFlag = document.createElement("img");
 
 	function countLoadedImageAndLaunchIfReady(){
 		picsToLoad--;
@@ -15,7 +17,10 @@
 		var imageList = [
 			{varName: carPic, theFile: "player1.png"},
 			{varName: trackPicRoad, theFile: "track_road.png"},
-			{varName: trackPicWall, theFile: "track_wall.png"}
+			{varName: trackPicWall, theFile: "track_wall.png"},
+			{varName: trackPicGoal, theFile: "track_goal.png"},
+			{varName: trackPicTree, theFile: "track_tree.png"},
+			{varName: trackPicFlag, theFile: "track_flag.png"}
 		];
 
 		picsToLoad = imageList.length;
