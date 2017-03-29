d04842a9dbd3c4a847ef8e267d60acb31a2a3655
richardyam1 <richardyam1@yahoo.com>
2017-03-29 14:57:40 -0400

    Transfer code from racing.  Deleted unnecessary code

---
+++ warriorLegend/js/main.js
@@ -0,0 +1,38 @@
+var p1 = new carClass();
+var canvas;
+var convasContext;
+
+$(document).ready(function(){
+
+	canvas = document.getElementById("gameCanvas");
+	canvasContext = canvas.getContext("2d");
+	
+	loadImages();
+});
+
+
+	function loadingDoneSoStartGame(){
+		var framesPerSecond = 30;
+		setInterval(function(){
+			drawEverything(); 
+			moveEverything();
+    	}, 1000/framesPerSecond);
+    	p1.carInit(carPic, "Blue Car");
+		initInput();
+	}
+
+	function drawEverything(){
+		//game board
+		drawTracks();
+
+		//draw car
+		p1.carDraw();
+
+	}
+
+	function moveEverything(){
+		p1.carMove();
+	}
+
+	
+		
