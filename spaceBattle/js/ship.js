f3f454706d6c3064a900d3d93ffb158f915ba03e
richardyam1 <richardyam1@yahoo.com>
2017-04-05 16:06:53 -0400

    Remove const REVERSE_POWER and any use of it.  Change variable names and values to something more suitable

--- spaceBattle/js/ship.js
+++ spaceBattle/js/ship.js
@@ -1,20 +1,16 @@
-	const GROUNDSPEED_DECAY_MULT = 0.94;
-	const DRIVE_POWER = 0.5;
-	const REVERSE_POWER = 0.2;
+	const SPACESPEED_DECAY_MULT = 0.99;
+	const THRUST_POWER = 0.15;
 	const TURN_RATE = 0.03;
-	const MIN_TURN_SPEED = 0.5;
 	//Use class so both cars have access to code below.
 	function shipClass (){
 		this.x = 75;
 		this.y = 75;
 		this.keyHeld_Gas = false;
-		this.keyHeld_Reverse = false;
 		this.keyHeld_TurnLeft = false;
 		this.keyHeld_TurnRight = false;
 
 		this.setupControls = function(forwardKey, backKey, leftKey, rightKey){
 				this.controlKeyForGas = forwardKey;
-				this.controlKeyForReverse = backKey;
 				this.controlKeyForTurnLeft = leftKey;
 				this.controlKeyForTurnRight = rightKey;
 			}
@@ -24,27 +20,25 @@
 		};
 
 		this.move = function(){
-			if(Math.abs(this.speed) >= MIN_TURN_SPEED){
+			
 				if(this.keyHeld_TurnLeft){
 					this.ang += -TURN_RATE*Math.PI;
 				}
 				if(this.keyHeld_TurnRight){
 					this.ang += TURN_RATE*Math.PI;
 				}
-			}
+			
 			if(this.keyHeld_Gas){
-				this.speed += DRIVE_POWER;
+				this.speed += THRUST_POWER;
 			}
-			if(this.keyHeld_Reverse){
-				this.speed += -REVERSE_POWER;
-			}
+			
 
 			 this.x += Math.cos(this.ang) * this.speed;
 			 this.y += Math.sin(this.ang) * this.speed;
 
 			
 			//slows down the car when key is not pressed
-			this.speed *= GROUNDSPEED_DECAY_MULT;
+			this.speed *= SPACESPEED_DECAY_MULT;
 			this.handleScreenWrap();
 		};
 		//reset car to starting position when goal line is reached
