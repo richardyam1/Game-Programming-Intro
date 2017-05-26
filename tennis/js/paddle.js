ee61a42cfefa2b1e984dc2a83c16770f1d618b2b
richardyam1 <richardyam1@yahoo.com>
2017-05-26 16:54:39 -0400

    Implement MAX_BALL_SPEED in PADDLE_THICKNESS calculation

--- tennis/js/paddle.js
+++ tennis/js/paddle.js
@@ -2,8 +2,8 @@ var paddle1Y = 250;
 var paddle2Y = 250;
 const PADDLE_HEIGHT = 100;
 //Make thickness greater than ball possible maximum speed to prevent ball from passing through paddle
-const PADDLE_THICKNESS = (ballSpeedX + 8) + 2;
-const DIST_FROM_EDGE = 110;
+const PADDLE_THICKNESS = MAX_BALL_SPEED + 2;
+const DIST_FROM_EDGE = 120;
 const cpuSpeed = 11;
 
 function moveComputerPaddle(){
