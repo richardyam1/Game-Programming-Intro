25ce4153765f48f0f9593d6b8be79427616aa9ce
richardyam1 <richardyam1@yahoo.com>
2017-06-09 17:17:38 -0400

    Replace paddle with paddle image

--- brickbreak/js/paddle.js
+++ brickbreak/js/paddle.js
@@ -2,3 +2,7 @@ const PADDLE_WIDTH = 100;
 const PADDLE_HEIGHT = 10;
 const PADDLE_Y = 540;
 var paddleX = 350;
+
+function paddleDraw(){
+	drawBitmapPositionedByTopLeftCorner(paddlePic, paddleX, PADDLE_Y)
+}
