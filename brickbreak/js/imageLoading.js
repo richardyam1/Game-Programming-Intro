1be27098bdc756fb01f7609f247e0212f0e6ecf4
richardyam1 <richardyam1@yahoo.com>
2017-06-22 10:56:23 -0400

    Swap brick colors

--- brickbreak/js/imageLoading.js
+++ brickbreak/js/imageLoading.js
@@ -1,10 +1,15 @@
 	var picsToLoad = 0;
 	var paddlePic = document.createElement("img");
 	var lifePic = document.createElement("img");
-	var brickPic = document.createElement("img");
+	var greyBrickPic = document.createElement("img");
+	var greenBrickPic = document.createElement("img");
+	var redBrickPic = document.createElement("img");
+	var yellowBrickPic = document.createElement("img");
+
+
 	var backgroundPic = document.createElement("img");
 	var ballPic = document.createElement("img");
-		
+	var brickPics = [];	
 
 	function countLoadedImageAndLaunchIfReady(){
 		picsToLoad--;
@@ -16,16 +21,26 @@
 	function loadImages(){
 		var imageList = [
 			{varName: paddlePic, theFile: "paddle.png"},
-			{varName: brickPic, theFile: "brick.png"},
 			{varName: lifePic, theFile: "life.png"},
 			{varName: backgroundPic, theFile: "bg.jpg"},
-			{varName: ballPic, theFile: "ball.png"}
+			{varName: ballPic, theFile: "ball.png"},
+			{varName: greyBrickPic, theFile: "brick1.png"},
+			{varName: greenBrickPic, theFile: "brick2.png"},
+			{varName: redBrickPic, theFile: "brick3.png"},
+			{varName: yellowBrickPic, theFile: "brick4.png"},
+
 		];
 
 		picsToLoad = imageList.length;
 
-		for(var i = 0; i < imageList.length; i++){			
-			beginLoadingImage(imageList[i].varName, imageList[i].theFile);	
+		for(var i = 0; i < imageList.length; i++){	
+			if(imageList[i].brickType !== undefined){
+				loadImageForBrickCode(imageList[i].brickType, imageList[i].theFile);
+			}		
+
+			else {
+				beginLoadingImage(imageList[i].varName, imageList[i].theFile);	
+			}
 		}
 		
 	}
@@ -33,6 +48,11 @@
 	function beginLoadingImage(imgVar, fileName){
 		imgVar.onload = countLoadedImageAndLaunchIfReady;
 		imgVar.src = "images/" + fileName;
+	}
+
+	function loadImageForBrickCode(brickCode, fileName){
+		brickPics[brickCode] = document.createElement("img");
+		beginLoadingImage(brickPics[brickCode], fileName);
 	}
 
 	
