8d7e74909302825bb5dedfe919b03f59d68963ae
richardyam1 <richardyam1@yahoo.com>
2017-10-04 15:16:11 -0400

    Add var for new images.

--- racing/js/track.js
+++ racing/js/track.js
@@ -1,77 +1,80 @@
-	const TRACK_W = 40;
-	const TRACK_H = 40;
-	const TRACK_COLS = 20;
-	const TRACK_ROWS = 15;
+const TRACK_W = 40;
+const TRACK_H = 40;
+const TRACK_COLS = 20;
+const TRACK_ROWS = 15;
 
 
-	var	trackGrid	=       [	4,	4,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	4,
-								4,	1,	1,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	1,	1,
-								1,	1,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	1,
-								1,	0,	0,	0,	0,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	0,	0,	0,	1,
-								1,	0,	0,	0,	1,	1,	1,	4,	4,	4,	4,	1,	1,	1,	1,	1,	1,	0,	0,	1,
-								1,	0,	0,	1,	1,	0,	0,	1,	4,	4,	1,	1,	0,	0,	0,	1,	1,	0,	0,	1,
-								1,	0,	0,	1,	0,	0,	0,	0,	1,	4,	1,	0,	0,	0,	0,	0,	1,	0,	0,	1,
-								1,	0,	0,	1,	0,	0,	0,	0,	0,	1,	1,	0,	0,	0,	0,	0,	1,	0,	0,	1,
-								1,	2,	2,	1,	0,	0,	0,	0,	0,	0,	1,	0,	0,	5,	0,	0,	1,	0,	0,	1,	
-								1,	0,	0,	1,	0,	0,	5,	0,	0,	0,	5,	0,	0,	1,	0,	0,	1,	0,	0,	1,
-								1,	1,	1,	1,	0,	0,	1,	1,	0,	0,	0,	0,	0,	1,	0,	0,	5,	0,	0,	1,
-								1,	1,	5,	1,	0,	0,	1,	1,	1,	0,	0,	0,	0,	1,	0,	0,	0,	0,	0,	1,
-								0,	3,	0,	0,	0,	0,	1,	4,	1,	1,	0,	0,	1,	1,	0,	0,	0,	0,	0,	1,
-								0,	3,	0,	0,	0,	0,	1,	4,	4,	1,	1,	1,	1,	1,	1,	0,	0,	0,	1,	1,
-								1,	1,	5,	1,	1,	1,	1,	4,	4,	4,	4,	4,	4,	1,	1,	1,	1,	1,	1,	1 ];
-	const TRACK_ROAD = 0;
-	const TRACK_WALL = 1;
-	const TRACK_PLAYER = 2;
-	const TRACK_GOAL = 3;
-	const TRACK_TREE = 4;
-	const TRACK_FLAG = 5;
+var	trackGrid	=       [	4,	4,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	4,
+							4,	1,	1,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	1,	1,
+							1,	1,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	1,
+							1,	0,	0,	0,	0,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	0,	0,	0,	1,
+							1,	0,	0,	0,	1,	1,	1,	4,	4,	4,	4,	1,	1,	1,	1,	1,	1,	0,	0,	1,
+							1,	0,	0,	1,	1,	0,	0,	1,	4,	4,	1,	1,	0,	0,	0,	1,	1,	0,	0,	1,
+							1,	0,	0,	1,	0,	0,	0,	0,	1,	4,	1,	0,	0,	0,	0,	0,	1,	0,	0,	1,
+							1,	0,	0,	1,	0,	0,	0,	0,	0,	1,	1,	0,	0,	0,	0,	0,	1,	0,	0,	1,
+							1,	2,	2,	1,	0,	0,	0,	0,	0,	0,	1,	0,	0,	5,	0,	0,	1,	0,	0,	1,	
+							1,	0,	0,	1,	0,	0,	5,	0,	0,	0,	5,	0,	0,	1,	0,	0,	1,	0,	0,	1,
+							1,	1,	1,	1,	0,	0,	1,	1,	0,	0,	0,	0,	0,	1,	0,	0,	5,	0,	0,	1,
+							1,	1,	5,	1,	0,	0,	1,	1,	1,	0,	0,	0,	0,	1,	0,	0,	0,	0,	0,	1,
+							0,	3,	0,	0,	0,	0,	1,	4,	1,	1,	0,	0,	1,	1,	0,	0,	0,	0,	0,	1,
+							0,	3,	0,	0,	0,	0,	1,	4,	4,	1,	1,	1,	1,	1,	1,	0,	0,	0,	1,	1,
+							1,	1,	5,	1,	1,	1,	1,	4,	4,	4,	4,	4,	4,	1,	1,	1,	1,	1,	1,	1 ];
+const TRACK_ROAD = 0;
+const TRACK_WALL = 1;
+const TRACK_PLAYER = 2;
+const TRACK_GOAL = 3;
+const TRACK_TREE = 4;
+const TRACK_FLAG = 5;
+const TRACK_GRASS = 6;
+const TRACK_OIL = 7;
 
-	function drawTracks(){
-		var trackIndex = 0;
-		var trackLeftEdgeX = 0;
-		var trackTopEdgeY = 0;
-		for(var eachRow = 0; eachRow < TRACK_ROWS; eachRow++){ // deal with one row at a time
 
-			trackLeftEdgeX = 0; // resetting horizontal draw position for tiles to left edge
+function drawTracks(){
+	var trackIndex = 0;
+	var trackLeftEdgeX = 0;
+	var trackTopEdgeY = 0;
+	for(var eachRow = 0; eachRow < TRACK_ROWS; eachRow++){ // deal with one row at a time
 
-			for(var eachCol = 0; eachCol < TRACK_COLS; eachCol++){ // left to right in each row
-				
-				var trackTypeHere = trackGrid[trackIndex];  //getting the track code here
-				canvasContext.drawImage(trackPics[trackTypeHere], trackLeftEdgeX, trackTopEdgeY);
-				trackIndex++; //increment which index we're going to next check in the track
-				trackLeftEdgeX += TRACK_W; // jump horizontal draw to next tile over by tile width
-			}
+		trackLeftEdgeX = 0; // resetting horizontal draw position for tiles to left edge
 
-			trackTopEdgeY += TRACK_H; // jump horizontal draw down by one tile height
+		for(var eachCol = 0; eachCol < TRACK_COLS; eachCol++){ // left to right in each row
+			
+			var trackTypeHere = trackGrid[trackIndex];  //getting the track code here
+			canvasContext.drawImage(trackPics[trackTypeHere], trackLeftEdgeX, trackTopEdgeY);
+			trackIndex++; //increment which index we're going to next check in the track
+			trackLeftEdgeX += TRACK_W; // jump horizontal draw to next tile over by tile width
 		}
 
+		trackTopEdgeY += TRACK_H; // jump horizontal draw down by one tile height
 	}
 
-	function getTrackAtPixelCoord(pixelX, pixelY){
-		var tileCol = pixelX/TRACK_W;
-		var tileRow = pixelY/TRACK_H;
+}
 
-		//round down to nearest whole number
-		tileCol = Math.floor(tileCol);
-		tileRow = Math.floor(tileRow);
+function getTrackAtPixelCoord(pixelX, pixelY){
+	var tileCol = pixelX/TRACK_W;
+	var tileRow = pixelY/TRACK_H;
 
-		//first check whether the car is within any part of the track wall
-		if(tileCol < 0 || tileCol >= TRACK_COLS || tileRow < 0 || tileRow >= TRACK_ROWS){
-			return TRACK_WALL; // bail out of function to avoid illegal array position usage
-		}
+	//round down to nearest whole number
+	tileCol = Math.floor(tileCol);
+	tileRow = Math.floor(tileRow);
 
-		var trackIndex = trackTileToIndex(tileCol, tileRow);
-		return trackGrid[trackIndex];
-		
+	//first check whether the car is within any part of the track wall
+	if(tileCol < 0 || tileCol >= TRACK_COLS || tileRow < 0 || tileRow >= TRACK_ROWS){
+		return TRACK_WALL; // bail out of function to avoid illegal array position usage
 	}
 
-	//returns index for the trackGrid
-	function trackTileToIndex(tileCol, tileRow){
-		return(tileCol + TRACK_COLS*tileRow);
-	}
+	var trackIndex = trackTileToIndex(tileCol, tileRow);
+	return trackGrid[trackIndex];
+	
+}
 
-	//check if there's a wall
-	function isWallAtTileCoord(trackTileCol, trackTileRow){
-		var trackIndex = trackTileCol + TRACK_COLS*trackTileRow;
-		return (trackGrid[trackIndex] == TRACK_WALL);
-	}
+//returns index for the trackGrid
+function trackTileToIndex(tileCol, tileRow){
+	return(tileCol + TRACK_COLS*tileRow);
+}
+
+//check if there's a wall
+function isWallAtTileCoord(trackTileCol, trackTileRow){
+	var trackIndex = trackTileCol + TRACK_COLS*trackTileRow;
+	return (trackGrid[trackIndex] == TRACK_WALL);
+}
