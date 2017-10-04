994076fed63eb5ca1fe29f0bda03d9386c17e2d4
richardyam1 <richardyam1@yahoo.com>
2017-10-04 16:56:07 -0400

    Add image set for night time

--- racing/js/imageLoading.js
+++ racing/js/imageLoading.js
@@ -13,18 +13,29 @@ function countLoadedImageAndLaunchIfReady(){
 }
 
 function loadImages(){
-	var imageList = [
-		{varName: carPic, theFile: "player1.png"},
-		{varName: car2Pic, theFile: "player2.png"},
-		{trackType: TRACK_ROAD, theFile: "track_road.png"},
-		{trackType: TRACK_WALL, theFile: "track_wall.png"},
-		{trackType: TRACK_GOAL, theFile: "track_goal.png"},
-		{trackType: TRACK_TREE, theFile: "track_tree.png"},
-		{trackType: TRACK_FLAG, theFile: "track_flag.png"},
-		{trackType: TRACK_GRASS, theFile: "track_grass.png"},
-		{trackType: TRACK_OIL, theFile: "track_oil.png"}
+	var imageList = [];
+	var imageListSet = [[
+		{varName: carPic, theFile: "player1_day.png"},
+		{varName: car2Pic, theFile: "player2_day.png"},
+		{trackType: TRACK_ROAD, theFile: "track_road_day.png"},
+		{trackType: TRACK_WALL, theFile: "track_wall_day.png"},
+		{trackType: TRACK_GOAL, theFile: "track_goal_day.png"},
+		{trackType: TRACK_TREE, theFile: "track_tree_day.png"},
+		{trackType: TRACK_FLAG, theFile: "track_flag_day.png"},
+		{trackType: TRACK_GRASS, theFile: "track_grass_day.png"},
+		{trackType: TRACK_OIL, theFile: "track_oil_day.png"}],
+		
+		[{varName: carPic, theFile: "player1_night.png"},
+		{varName: car2Pic, theFile: "player2_night.png"},
+		{trackType: TRACK_ROAD, theFile: "track_road_night.png"},
+		{trackType: TRACK_WALL, theFile: "track_wall_night.png"},
+		{trackType: TRACK_GOAL, theFile: "track_goal_night.png"},
+		{trackType: TRACK_TREE, theFile: "track_tree_night.png"},
+		{trackType: TRACK_FLAG, theFile: "track_flag_night.png"},
+		{trackType: TRACK_GRASS, theFile: "track_grass_night.png"},
+		{trackType: TRACK_OIL, theFile: "track_oil_night.png"}]
 	];
-
+	imageList = imageListSet[currentTime]; 
 	picsToLoad = imageList.length;
 
 	for(var i = 0; i < imageList.length; i++){
